7a0df2d2a147485e7c41e6ddae5c3828c87e5747
Joel Kram <joelnskra@gmail.com>
2025-03-03 19:22:14 +0000

    add game state management and win checking logic to TicTacToe

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const gameboard = {
   board: [[" ", " ", " "],[" ", " ", " "], [" ", " ", " "]],
-  
+  gameWon: false,
+  turn: 0,
+
   updateBoard: function(){
     console.log("   ") // empty space before a board
     this.board.forEach((arr, i)=> { // looping over each array in the board
@@ -14,6 +16,16 @@ const gameboard = {
     } else {
       console.log("invalid, please try again")
     }
+  },
+
+  checkForWin: function(player_symbol) {
+    for (let i = 0; i < this.board.length; i++) {
+      let row = this.board[i];
+      let col = this.board.map(row => row[i]);
+      if (row.every(cell => cell === player_symbol) || col.every(cell => cell === player_symbol)) { //check lines
+        this.gameWon = true;
+      }
+    }
   }
 
 
@@ -21,15 +33,23 @@ const gameboard = {
 }
 
 const player = {
-  init: function(x, y, symbol) {
+  init: function(x, y) {
     this.x = x;
     this.y = y;
-    this.symbol = symbol;
+    this.symbol = input;
+  },
+
+  whoIsPlaying: function() {
+    let input;
+    input = prompt("Enter X or O");
+    return input;
   },
 
   playerMove: function(x, y) {
-   this.symbol = board[x][y];
+   this.symbol = gameboard.board[x][y];
   }
+
+  
 }
 //debug
 gameboard.updateBoard();
